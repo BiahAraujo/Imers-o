23ab3256ea1f623aaac727c1e21017d3c8facafc
Bianca Alves Araujo <101605070+BiahAraujo@users.noreply.github.com>
2024-09-07 15:54:48 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: "The Walking Dead",
+        descricao: "The Walking Dead é uma série de televisão dramática e pós-apocalíptica norte-americana, desenvolvida por Frank Darabont, e baseada na série em quadrinhos de mesmo nome de Robert Kirkman, Tony Moore e Charlie Adlard.",
+        link: "https://pt.wikipedia.org/wiki/The_Walking_Dead_(s%C3%A9rie_de_televis%C3%A3o)",
+        tags: "drama série"
+    },
+    {
+        titulo: "Grey's Anatomy",
+        descricao: "Grey's Anatomy é uma série de televisão dramática médica norte-americana, criada por Shonda Rhimes. A série acompanha a vida pessoal e profissional de Meredith Grey e de um grupo de jovens médicos residentes no Seattle Grace Hospital.",
+        link: "https://pt.wikipedia.org/wiki/Grey's_Anatomy",
+        tags: "medico hospital vida série"
+    },
+    {
+        titulo: "The Big Bang Theory",
+        descricao: "The Big Bang Theory é uma sitcom norte-americana que gira em torno de um grupo de amigos, todos cientistas ou engenheiros, que trabalham em um instituto de pesquisa teórica em Pasadena, Califórnia.",
+        link: "https://pt.wikipedia.org.wiki/The_Big_Bang_Theory",
+        tags: "amigo ciencia trabalho série"
+    }
+
+];
+
